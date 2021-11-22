3479e8235dcc92c6f5763eed8631cf01b0c6aca9
MardouSilva <mardousilva@gmail.com>
2021-11-22 09:30:09 -0300

    Aula de javascript 22-11-2021

---
+++ javascript/aula-22-11-2021/script.js
@@ -0,0 +1,53 @@
+//Editando cabeçalho
+
+//Minha foto
+document.querySelector("#foto").setAttribute = "src"
+document.querySelector("#foto").src="imagens/foto.jpg"
+
+//Informações pessoais
+document.querySelector("#nome").innerHTML="Marcos Douglas"
+document.querySelector("#sobre").innerHTML="Estudante de 20 anos, solteiro."
+
+//Conteúdo principal da pagina
+
+//Informações gerais
+document.querySelector("#tema").innerHTML="Meu gosto musical"
+document.querySelector("#cantor").innerHTML="Meu cantor favorito: Enygma"
+document.querySelector("#genero").innerHTML="Gênero musical: Músicas Geek"
+
+//lista de músicas
+document.querySelector("#lista").innerHTML="Músicas Preferidas"
+
+document.querySelector("#musica1").innerHTML="Um milhão de sonhos | Enygma 78"
+document.querySelector("#musica1").setAttribute="href"
+document.querySelector("#musica1").setAttribute="target"
+document.querySelector("#musica1").href="https://www.youtube.com/watch?v=OybrO4_rpyY"
+document.querySelector("#musica1").target="_blanck"
+
+document.querySelector("#musica2").innerHTML="See you, Space Cowboy | Enygma 79"
+document.querySelector("#musica2").setAttribute="href"
+document.querySelector("#musica2").setAttribute="target"
+document.querySelector("#musica2").href="https://www.youtube.com/watch?v=CfdVHkIzZpw"
+document.querySelector("#musica2").target="_blanck"
+
+document.querySelector("#musica3").innerHTML="Coração humano | Enygma 84"
+document.querySelector("#musica3").setAttribute="href"
+document.querySelector("#musica3").setAttribute="target"
+document.querySelector("#musica3").href="https://www.youtube.com/watch?v=SnpLuQ5NWYQ"
+document.querySelector("#musica3").target="_blanck"
+
+document.querySelector("#musica4").innerHTML="Rebelião | Enygma 90"
+document.querySelector("#musica4").setAttribute="href"
+document.querySelector("#musica4").setAttribute="target"
+document.querySelector("#musica4").href="https://www.youtube.com/watch?v=Pk3o9mI6Xzk"
+document.querySelector("#musica4").target="_blanck"
+
+document.querySelector("#musica5").innerHTML="O monstro sem nome | Enygma 99"
+document.querySelector("#musica5").setAttribute="href"
+document.querySelector("#musica5").setAttribute="target"
+document.querySelector("#musica5").href="https://www.youtube.com/watch?v=aRyo78ECpmw&t=231s"
+document.querySelector("#musica5").target="_blanck"
+
+//Imagem do canal
+document.querySelector("#imagem").setAttribute="src"
+document.querySelector("#imagem").src="imagens/canal.png"
